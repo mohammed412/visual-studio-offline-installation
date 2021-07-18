b89e046765fd49d3c2c1b2b6897a3bb49731822e
Mourad ouchane <mouradouchane@gmail.com>
2021-07-18 18:18:56 +0100

    base:new project just putting project base :)

---
+++ visual_studio_downloader.py
@@ -0,0 +1,20 @@
+
+try:
+    import httplib
+except:
+    import http.client as httplib
+
+def have_internet():
+    conn = httplib.HTTPConnection("www.google.com", timeout=5)
+    try:
+        conn.request("HEAD", "/")
+        conn.close()
+        print("network accessed")
+        return True
+    except:
+        conn.close()
+        print("network denied")
+        return False
+
+if not have_internet() : exit()
+print("end ...")
